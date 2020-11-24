b3a00650ff83601c858e6e7e67c68b7ae0e5b3ef
KimJiHo <shield1203@naver.com>
2020-11-24 22:34:39 +0900

    DB정보제거, env

--- ArKnights.js
+++ ArKnights.js
@@ -2,9 +2,10 @@ var express = require('express')
 var app = express();
 
 app.use(express.json())
+require('dotenv').config();
 
 var router = require('./router/main')(app);
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT, function(){
     console.log('Server start : project ArKnight');
 })
